169c82114f75af5510c3d1d86f638d588c45184d
pratikgithub123 <dangipratik19@gmail.com>
2021-09-27 19:09:17 +0545

    register2

--- login_reg.py
+++ login_reg.py
@@ -10,3 +10,22 @@ def register():
     register_screen = Toplevel(main_screen)
     register_screen.title("Register")
     register_screen.geometry("310x230")
+    global username
+    global password
+    global username_entry
+    global password_entry
+    username = StringVar()
+    password = StringVar()
+    Label(register_screen, text="Please enter details below", bg="orange", font=("Times", "15", "bold italic")).pack()
+    Label(register_screen, text="").pack()
+    username_lable = Label(register_screen, text="Username  ", font=("Times", "12", "bold"))
+    username_lable.pack()
+    username_entry = Entry(register_screen, textvariable=username, font=("Times", "12", "bold"))
+    username_entry.pack()
+    password_lable = Label(register_screen, text="Password  ", font=("Times", "12", "bold"))
+    password_lable.pack()
+    password_entry = Entry(register_screen, textvariable=password, font=("Times", "12", "bold"))
+    password_entry.pack()
+    Label(register_screen, text="").pack()
+    Button(register_screen, text="Register", width=10, height=1, font=("Times", "12", "bold"),
+           command=register_user).pack()
