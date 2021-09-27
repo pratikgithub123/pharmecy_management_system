6dab46f61bfc9762d81f5f03e98d98070f51c242
pratikgithub123 <dangipratik19@gmail.com>
2021-09-27 21:32:06 +0545

    exit

--- pharmecy_management_system/management_sys.py
+++ pharmecy_management_system/management_sys.py
@@ -1,3 +1,43 @@
+# module import
+# module imports
+
+from tkinter import *
+import os
+from tkinter import messagebox
+from management_sys import login_page
+def register():
+    global register_screen
+    register_screen = Toplevel(main_screen)
+    register_screen.title("Register")
+    register_screen.geometry("310x230")
+    global username
+    global password
+    global username_entry
+    global password_entry
+    username = StringVar()
+    password = StringVar()
+    Label(register_screen, text="Please enter details below", bg="orange",font=("Times", "15", "bold italic")).pack()
+    Label(register_screen, text="").pack()
+    username_lable = Label(register_screen, text="Username  ",font=("Times", "12", "bold"))
+    username_lable.pack()
+    username_entry = Entry(register_screen, textvariable=username,font=("Times", "12", "bold"))
+    username_entry.pack()
+    password_lable = Label(register_screen, text="Password  ",font=("Times", "12", "bold"))
+    password_lable.pack()
+    password_entry = Entry(register_screen, textvariable=password,font=("Times", "12", "bold"))
+    password_entry.pack()
+    Label(register_screen, text="").pack()
+    Button(register_screen, text="Register", width=10, height=1 ,font=("Times", "12", "bold"), command=register_user).pack()
+def login():
+    global login_screen
+    login_screen = Toplevel(main_screen)
+    login_screen.title("Login")
+    login_screen.geometry("310x230")
+    Label(login_screen, text="Enter the details  to login",font=("Times", "15", "bold italic")).pack()
+    Label(login_screen, text="").pack()
+    global username_verify
+ 4  management_sys.py
+@@ -1,328 +1,328 @@
 #imports
 from tkinter import *
 from PIL import Image,ImageTk
@@ -239,3 +279,8 @@ def login_page():
         # Create a update button
         edit_btn = Button(editor, text=" SAVE ", command=update)
         edit_btn.grid(row=12, column=0, columnspan=2, pady=10, padx=10, ipadx=125)
+    def exit_fun():
+        ask = messagebox.askyesno("exit","Do you want to exit?")
+        if ask == 1 :
+            root.destroy()
+
